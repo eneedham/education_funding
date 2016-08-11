4d55ddfe8778cba65e3b24f8641a8d0cae5daa01
eneedham <eneedham@design.upenn.edu>
2016-08-11 11:23:33 -0400

    changing carto basemap to https

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function main() {
   //   attribution: 'Stamen'
   // }).addTo(map);
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{basemap-name}/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: 'abcd',
   }).addTo(map);
